383ce359232241395bb4d8ae5e0f302aafd002dd
RichardJECooke <RichardJECooke@protonmail.com>
2025-08-08 17:25:11 +0200

    Put huge number of todos back

--- seed.sql
+++ seed.sql
@@ -1,15 +1,15 @@
 -- ============================
--- Insert 1000 parent todos
+-- Insert 10,000 parent todos
 -- ============================
 INSERT INTO "TodoItems" ("Title", "IsDone", "ParentId")
 SELECT
   'Todo #' || g AS "Title",
   false AS "IsDone",
   NULL AS "ParentId"
-FROM generate_series(1, 1000) g;
+FROM generate_series(1, 10000000) g;
 
 -- ============================
--- Insert 10 comments per todo
+-- Insert 10 comments per todo (100,000 total)
 -- ============================
 INSERT INTO "Comments" ("Content", "CreatedAt", "TodoItemId")
 SELECT
